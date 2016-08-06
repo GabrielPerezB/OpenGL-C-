3e170b0ddafacff5b0f2374614ec9e0c0481d977
GabrielPerezB <gabrielapb1996@gmail.com>
2016-08-06 16:48:12 -0600

    Draw circle and testing joystick

--- OpenGL_C++/OpenGL_C++/main.cpp
+++ OpenGL_C++/OpenGL_C++/main.cpp
@@ -1,5 +1,17 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <math.h>
+#include <iostream>
+#include <cstring>
+
+#define M_PI       3.14159265358979323846   // pi
+#define SCREEN_WIDTH 600
+#define SCREEN_HEIGHT 400
+
+
+void drawCircle(GLfloat x, GLfloat y, GLfloat z, GLfloat radius, GLint numberOfSides);
+
+
 
 int main(void) {
 	GLFWwindow *window;
@@ -12,7 +24,7 @@ int main(void) {
 
 	//create a windiws mode and its OpenGL context
 
-	window = glfwCreateWindow(600, 400, "OpenGL and C++ Proyect", NULL, NULL);
+	window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "OpenGL and C++ Proyect", NULL, NULL);
 
 	if (!window)
 	{
@@ -21,17 +33,80 @@ int main(void) {
 	}
 
 	//Make window's context current
+	glfwMakeContextCurrent(window);
+
+	glViewport(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT);// specifies the part of the window to which OpenGL will draw (in pixels), convert from normalised to pixels
+	glMatrixMode(GL_PROJECTION); // projection matrix defines the properties of the camera that views the objects in the world coordinate frame. Here you typically set the zoom factor, aspect ratio and the near and far clipping planes
+	glLoadIdentity(); // replace the current matrix with the identity matrix and starts us a fresh because matrix transforms such as glOrpho and glRotate cumulate, basically puts us at (0, 0, 0)
+	glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1); // essentially set coordinate system
+	glMatrixMode(GL_MODELVIEW); // (default matrix mode) modelview matrix defines how your objects are transformed (meaning translation, rotation and scaling) in your world
+	glLoadIdentity(); // same as above comment
 
 	//loop intil the user closes the window
 	while (! glfwWindowShouldClose(window))
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
-		//render the open gl here
+
+		//x max 570, min 30
+		//y max 370, min 30
+		drawCircle(30,30, 0, 30, 360);
+		
+		//dectect joystick
+		int present = glfwJoystickPresent(GLFW_JOYSTICK_1);
+		//printf ( "Joystick status:  %d \n" , present );
+
+		if (1 == present) {
+			int axesCount;
+			const float *axes = glfwGetJoystickAxes(GLFW_JOYSTICK_1, &axesCount);
+			printf("Number of axes avaliable: %d \n", axesCount);
+		}
+
 
 		//sweap front and back buffers
 		glfwSwapBuffers(window);
-		//poll for and process events
 
+		//poll for and process events
 		glfwPollEvents();
 	}
+
+	glfwTerminate();
+	return 0;
+}
+
+
+
+void drawCircle(GLfloat x, GLfloat y, GLfloat z, GLfloat radius, GLint numberOfSides)
+{
+	int numberOfVertices = numberOfSides + 2;
+
+	GLfloat twicePi = 2.0f * M_PI;
+
+	GLfloat circleVerticesX[362];
+	GLfloat circleVerticesY[362];
+	GLfloat circleVerticesZ[362];
+
+	circleVerticesX[0] = x;
+	circleVerticesY[0] = y;
+	circleVerticesZ[0] = z;
+
+	for (int i = 1; i < numberOfVertices; i++)
+	{
+		circleVerticesX[i] = x + (radius * cos(i *  twicePi / numberOfSides));
+		circleVerticesY[i] = y + (radius * sin(i * twicePi / numberOfSides));
+		circleVerticesZ[i] = z;
+	}
+
+	GLfloat allCircleVertices[(362)* 3];
+
+	for (int i = 0; i < numberOfVertices; i++)
+	{
+		allCircleVertices[i * 3] = circleVerticesX[i];
+		allCircleVertices[(i * 3) + 1] = circleVerticesY[i];
+		allCircleVertices[(i * 3) + 2] = circleVerticesZ[i];
+	}
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glVertexPointer(3, GL_FLOAT, 0, allCircleVertices);
+	glDrawArrays(GL_TRIANGLE_FAN, 0, numberOfVertices);
+	glDisableClientState(GL_VERTEX_ARRAY);
 }
