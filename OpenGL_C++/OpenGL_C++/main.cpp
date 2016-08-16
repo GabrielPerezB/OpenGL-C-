db252218c4d3ab257d1f74c962fbcc2f40bcdc7c
GabrielPerezB <gabrielapb1996@gmail.com>
2016-08-15 22:33:08 -0600

    Rotating sphere and joystick implementation

--- OpenGL_C++/OpenGL_C++/main.cpp
+++ OpenGL_C++/OpenGL_C++/main.cpp
@@ -1,112 +1,217 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <GL/glut.h>
 #include <math.h>
 #include <iostream>
 #include <cstring>
 
 #define M_PI       3.14159265358979323846   // pi
-#define SCREEN_WIDTH 600
-#define SCREEN_HEIGHT 400
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
 
 
-void drawCircle(GLfloat x, GLfloat y, GLfloat z, GLfloat radius, GLint numberOfSides);
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+GLfloat xRotated, yRotated, zRotated;
+GLdouble radius = 1;
+/*
+	max x = -350        max y = 250                 min x =     350                     min y = -250
+*/
+int positionX = 0;
+int positionY = 0;
+int option = -1;
 
+void redisplayFunc(void)
+{
 
-int main(void) {
-	GLFWwindow *window;
+	glMatrixMode(GL_MODELVIEW);
+	glViewport(positionX,positionY,SCREEN_WIDTH,SCREEN_HEIGHT);
+	// clear the drawing buffer.
+	glClear(GL_COLOR_BUFFER_BIT);
 
-	//initialise glfw
-	if (!glfwInit())
+	// clear the identity matrix.
+	glLoadIdentity();
+
+	// traslate the draw by z = -4.0
+	// Note this when you decrease z like -8.0 the drawing will looks far , or smaller.
+	glTranslatef(0.0, 0.0, -20.0);
+
+	// Red color used to draw.
+	glColor3f(0.8, 0.2, 0.1);//0.8, 0.2, 0.1
+
+	// changing in transformation matrix.
+	// rotation about X axis
+	glRotatef(xRotated, 1.0, 0.0, 0.0);
+	// rotation about Y axis
+	glRotatef(yRotated, 0.0, 1.0, 0.0);
+	// rotation about Z axis
+	glRotatef(zRotated, 0.0, 0.0, 1.0);
+
+	// scaling transfomation 
+	glScalef(1.0, 1.0, 1.0);
+
+	// built-in (glut library) function , draw you a sphere.
+	glutSolidSphere(radius, 20, 20);
+
+	// Flush buffers to screen
+	glFlush();
+	// sawp buffers called because we are using double buffering 
+	 glutSwapBuffers();
+}
+
+void reshapeFunc(int x, int y)
+{
+	if (y == 0 || x == 0) return;  //Nothing is visible then, so return
+								   //Set a new projection matrix
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	//Angle of view:40 degrees
+	//Near clipping plane distance: 0.5
+	//Far clipping plane distance: 20.0
+
+	gluPerspective(40.0, (GLdouble)x / (GLdouble)y, 0.5, 20.0);
+	glMatrixMode(GL_MODELVIEW);
+	glViewport(positionX, positionY, x, y);  //Use the whole window for rendering
+}
+
+void idleFunc(void)
+{
+	if (option == 0)
 	{
-		return -1;
+		yRotated -= 0.1;
 	}
-
-	//create a windiws mode and its OpenGL context
-
-	window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "OpenGL and C++ Proyect", NULL, NULL);
-
-	if (!window)
+	if (option == 1)
 	{
-		glfwTerminate();
-		return -1;
+		yRotated += 0.1;
 	}
-
-	//Make window's context current
-	glfwMakeContextCurrent(window);
-
-	glViewport(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT);// specifies the part of the window to which OpenGL will draw (in pixels), convert from normalised to pixels
-	glMatrixMode(GL_PROJECTION); // projection matrix defines the properties of the camera that views the objects in the world coordinate frame. Here you typically set the zoom factor, aspect ratio and the near and far clipping planes
-	glLoadIdentity(); // replace the current matrix with the identity matrix and starts us a fresh because matrix transforms such as glOrpho and glRotate cumulate, basically puts us at (0, 0, 0)
-	glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1); // essentially set coordinate system
-	glMatrixMode(GL_MODELVIEW); // (default matrix mode) modelview matrix defines how your objects are transformed (meaning translation, rotation and scaling) in your world
-	glLoadIdentity(); // same as above comment
-
-	//loop intil the user closes the window
-	while (! glfwWindowShouldClose(window))
+	if (option == 2)
 	{
-		glClear(GL_COLOR_BUFFER_BIT);
+		xRotated -= 0.1;
+	}
+	if (option == 3)
+	{
+		xRotated += 0.1;
+	}
+	redisplayFunc();
+}
 
-		//x max 570, min 30
-		//y max 370, min 30
-		drawCircle(30,30, 0, 30, 360);
-		
-		//dectect joystick
-		int present = glfwJoystickPresent(GLFW_JOYSTICK_1);
-		//printf ( "Joystick status:  %d \n" , present );
+void joystick(unsigned int buttonmask, int x, int y, int z)
+{
 
-		if (1 == present) {
-			int axesCount;
-			const float *axes = glfwGetJoystickAxes(GLFW_JOYSTICK_1, &axesCount);
-			printf("Number of axes avaliable: %d \n", axesCount);
+
+	if (buttonmask & GLUT_JOYSTICK_BUTTON_D) {
+		xRotated = 270;
+		option = 0;
+		if (positionX - 5 >= -350) {
+			positionX -= 5;
 		}
-
-
-		//sweap front and back buffers
-		glfwSwapBuffers(window);
-
-		//poll for and process events
-		glfwPollEvents();
+	}
+	if (buttonmask & GLUT_JOYSTICK_BUTTON_A) {
+		yRotated = 270;
+		option = 2;
+		if (positionY + 5 <= 250)
+		{
+			positionY += 5;
+		}
 	}
 
-	glfwTerminate();
-	return 0;
+	if (buttonmask & GLUT_JOYSTICK_BUTTON_B) {
+		xRotated = 270;
+		option = 1;
+		if (positionX + 5 <= 350)
+		{
+			positionX += 5;
+		}
+	}
+
+	if (buttonmask & GLUT_JOYSTICK_BUTTON_C) {
+		yRotated = 270;
+		option = 3;
+		if (positionY - 5 >= -250)
+		{
+			positionY -= 5;
+		}
+	}
+}
+
+void keyboard(int key, int x, int y)
+{
+	switch (key)
+	{
+	case 100:
+		//left
+		printf("button pressed: left \n");
+		xRotated = 270;
+		option = 0;
+		if (positionX - 5 >= -350) {
+			positionX -= 5;
+		}
+		break;
+	case 101:
+		//up
+		printf("button pressed: up \n");
+		yRotated = 270;
+		option = 2;
+		if (positionY + 5 <=250)
+		{
+			positionY += 5;
+		}
+		break;
+	case 102:
+		//right
+		printf("button pressed: right \n");
+		xRotated = 270;
+		option = 1;
+		if (positionX + 5 <= 350)
+		{
+			positionX += 5;
+		}
+		break;
+	case 103:
+		//down
+		printf("button pressed: down \n");
+		yRotated = 270;
+		option = 3;
+		if (positionY-5 >=-250)
+		{
+			positionY -= 5;
+		}
+		break;
+	case 32:
+		//space
+		printf("button pressed: space \n");
+		exit(0);
+		break;
+	}
+
 }
 
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main( int argc, char **argv) {
+	
 
-void drawCircle(GLfloat x, GLfloat y, GLfloat z, GLfloat radius, GLint numberOfSides)
-{
-	int numberOfVertices = numberOfSides + 2;
-
-	GLfloat twicePi = 2.0f * M_PI;
-
-	GLfloat circleVerticesX[362];
-	GLfloat circleVerticesY[362];
-	GLfloat circleVerticesZ[362];
-
-	circleVerticesX[0] = x;
-	circleVerticesY[0] = y;
-	circleVerticesZ[0] = z;
-
-	for (int i = 1; i < numberOfVertices; i++)
-	{
-		circleVerticesX[i] = x + (radius * cos(i *  twicePi / numberOfSides));
-		circleVerticesY[i] = y + (radius * sin(i * twicePi / numberOfSides));
-		circleVerticesZ[i] = z;
-	}
-
-	GLfloat allCircleVertices[(362)* 3];
-
-	for (int i = 0; i < numberOfVertices; i++)
-	{
-		allCircleVertices[i * 3] = circleVerticesX[i];
-		allCircleVertices[(i * 3) + 1] = circleVerticesY[i];
-		allCircleVertices[(i * 3) + 2] = circleVerticesZ[i];
-	}
-
-	glEnableClientState(GL_VERTEX_ARRAY);
-	glVertexPointer(3, GL_FLOAT, 0, allCircleVertices);
-	glDrawArrays(GL_TRIANGLE_FAN, 0, numberOfVertices);
-	glDisableClientState(GL_VERTEX_ARRAY);
+	//Initialize GLUT
+	glutInit(&argc, argv);
+	//double buffering used to avoid flickering problem in animation
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); 
+	// window size
+	glutInitWindowSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+	// create the window 
+	glutCreateWindow("OpenGL and C++ Proyect");
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	xRotated = yRotated = zRotated = 0;//30.0
+	xRotated = 0;//33
+	yRotated = 0;//40
+	glClearColor(0.2, 0.2, 0.2, 0.2); 
+	//Assign  the function used in events
+	glutDisplayFunc(redisplayFunc);
+	glutReshapeFunc(reshapeFunc);
+	glutIdleFunc(idleFunc);
+	//glutSpecialFunc(keyboard);
+	glutJoystickFunc(joystick,25);
+	//Let start glut loop
+	glutMainLoop(); 
+	
+	return 0;
 }
