846b882cd2fa568f72465fedabd85208ae458d3c
GabrielPerezB <gabrielapb1996@gmail.com>
2016-09-28 09:24:06 -0600

    first commit

---
+++ Csharp_Prolog/Csharp_Prolog/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+            Lenguajes de programación 
+            Project: Battleship with C# and Prolog
+            Students: Gabriel Pérez Barquero
+                      Cristian Alvarado Gomez
+
+*/
+namespace Csharp_Prolog
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
